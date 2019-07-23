34d2b6863af5630e5509fa495fd947cedeadf3e6
Justin Neil <justinmneil@gmail.com>
2019-07-23 13:49:10 -0400

    Initial commit

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,20 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for Dictionary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dictionary.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello, everybody!"
+  end
+end
+
+# Dictionary.hello()
